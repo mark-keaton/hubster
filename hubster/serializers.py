98adf256db0275556fed499836de1bc140377696
Mark Keaton <mkeaton@us.ci.org>
2019-05-03 06:43:11 -0600

    Fix model fields. Add serializers for all types. Add two serializers for Repo.

--- hubster/serializers.py
+++ hubster/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from hubster.models import GithubUser
+from hubster.models import GithubUser, GithubRepo, License
 
 
 class GithubUserSerializer(serializers.ModelSerializer):
@@ -8,3 +8,33 @@ class GithubUserSerializer(serializers.ModelSerializer):
         model = GithubUser
         fields = "__all__"
 
+
+class LicenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = License
+        fields = "__all__"
+
+
+class GithubRepoSerializer(serializers.ModelSerializer):
+    """
+    GithubRepo serializer/deserializer for working with
+    nested objects for License and Owner
+    """
+
+    license = LicenseSerializer()
+    owner = GithubUserSerializer()
+
+    class Meta:
+        model = GithubRepo
+        fields = "__all__"
+        depth = 1
+
+
+class GithubRepoSerializerWithId(GithubRepoSerializer):
+    """
+    GithubRepo serializer/deserializer for working with
+    integer values representing foreign keys to License and Owner.
+    """
+
+    license = serializers.PrimaryKeyRelatedField(queryset=License.objects.all())
+    owner = serializers.PrimaryKeyRelatedField(queryset=GithubUser.objects.all())
