d86daa39e53342f9411e85850a957c41ce0c4b9c
Mark Keaton <mkeaton@us.ci.org>
2019-05-05 15:21:33 -0500

    Add furl. Get async structure set up.

--- scraper.py
+++ scraper.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python
-
-import aiohttp
 import argparse
 import asyncio
-import os
+from string import Template
+
+import aiohttp
 import django
+import os
+from furl import furl
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hubster.settings")
 django.setup()
 
-from hubster.serializers import GithubRepoSerializerWithId
+from hubster.models import GithubUser
+from hubster.serializers import GithubRepoSerializerWithId, GithubUserSerializer
+
+USER_URL_BASE = "https://api.github.com/users"
+USER_REPO_TEMPLATE = Template("https://api.github.com/user/$user/repos")
 
 
-async def scrape(concurrency, buffer, quantity) -> None:
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://api.github.com/events") as resp:
-            print(resp.status)
-            print(await resp.text())
+async def scrapeUsers(session: aiohttp.ClientSession, startId: int = None) -> None:
+    if not startId:
+        maxId = GithubUser.objects.values("id").order_by("-id").first()
+        startId = maxId.get("id")  # In case the
+
+    queryArgs = {"since": startId} if startId else {}
+
+    usersUrl = furl(USER_URL_BASE).add(queryArgs).url
+    async with session.get(usersUrl) as resp:
+        print(resp.status)
+        print(await resp.text())
 
 
-async def main() -> None:
+async def scrape(
+    session: aiohttp.ClientSession,
+    concurrency: int,
+    buffer: int,
+    quantity: int,
+    startId: int = None,
+) -> None:
+    await scrapeUsers(session, startId)
+
+
+async def main(loop: asyncio.AbstractEventLoop) -> None:
     parser = argparse.ArgumentParser(description="Hubster Github scraper")
 
     parser.add_argument(
@@ -45,16 +67,27 @@ async def main() -> None:
         "--quantity",
         action="store",
         dest="quantity",
-        default=100,
+        default=3,
         type=int,
-        help="Total number of repos to scrape before quitting",
+        help="Total number of users to scrape before quitting",
+    )
+    parser.add_argument(
+        "-s",
+        "--start",
+        action="store",
+        dest="quantity",
+        type=int,
+        help="User ID to start scraping repos from (exclusive), e.g., 46 will start scraping at 47. By default, it will start from the max ID in the database.",
     )
 
     args = vars(parser.parse_args())
-    await scrape(**args)
+
+    conn = aiohttp.TCPConnector(limit=3)
+    async with aiohttp.ClientSession(connector=conn, loop=loop) as session:
+        await scrape(session, **args)
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main(loop))
 
